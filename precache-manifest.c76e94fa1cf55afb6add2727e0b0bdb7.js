58752a769bd415c2ec3362e219f1978dd31c3ac0
Naman-ap <141435865+Naman-ap@users.noreply.github.com>
2024-12-09 01:56:34 +0530

    Updates

---
+++ precache-manifest.c76e94fa1cf55afb6add2727e0b0bdb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5390fe45560b883d456c7d09254cd712",
+    "url": "/AlgoFlow/index.html"
+  },
+  {
+    "revision": "6dd3d8b1b6a6d82d33e7",
+    "url": "/AlgoFlow/static/css/main.1f806fdd.chunk.css"
+  },
+  {
+    "revision": "db4474e7572b8c3b7041",
+    "url": "/AlgoFlow/static/js/2.64520bdb.chunk.js"
+  },
+  {
+    "revision": "6dd3d8b1b6a6d82d33e7",
+    "url": "/AlgoFlow/static/js/main.6d0d921c.chunk.js"
+  },
+  {
+    "revision": "263c92cde4ebd590d923",
+    "url": "/AlgoFlow/static/js/runtime-main.cbb1986b.js"
+  }
+]);
